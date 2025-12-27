b163fbfa3ca7117fb326d0066d937ef81c59545e
Srinu-Mylapalli <srinivasmylapali@gmail.com>
2025-12-27 18:50:21 +0530

    java prgms

---
+++ methodsPrgms/Demo.java
@@ -0,0 +1,16 @@
+// Creating method by using object
+class Demo
+{
+	void test()
+	{
+		System.out.println("welcome ");
+	}
+
+	public static void main(String[] args)
+	{
+		Demo s= new Demo();
+		s.test();
+		System.out.println("to java programming");
+	}
+}
+
---
+++ typesOfVariables/InstanceVar.java
@@ -0,0 +1,9 @@
+class InstanceVar
+{
+	int std_id = 101;// Instance variable
+	public static void main(String[] args)
+	{
+		InstanceVar std = new InstanceVar();
+		System.out.println("Id is :"+std.std_id);
+	}
+}
